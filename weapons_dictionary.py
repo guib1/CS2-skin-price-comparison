1793048029fc191f98a808cbc032301e11c29531
Guilherme Rodrigues <guibnuevo@gmail.com>
2025-05-29 01:12:24 -0300

    feat: some brand new things

---
+++ weapons_dictionary.py
@@ -0,0 +1,48 @@
+cs2_weapons_data = {
+    'Knives': {
+        'Bayonet': ['Fade', 'Doppler', 'Tiger Tooth', 'Lore', 'Gamma Doppler'],
+        'Karambit': ['Fade', 'Doppler', 'Crimson Web', 'Slaughter', 'Case Hardened'],
+        'Butterfly Knife': ['Fade', 'Doppler', 'Slaughter', 'Crimson Web', 'Ultraviolet'],
+        'Huntsman Knife': ['Fade', 'Doppler', 'Crimson Web', 'Night'],
+        'Gut Knife': ['Fade', 'Doppler', 'Marble Fade', 'Ruby', 'Sapphire'],
+        'Flip Knife': ['Fade', 'Doppler', 'Crimson Web', 'Case Hardened'],
+        'Shadow Daggers': ['Fade', 'Doppler', 'Marble Fade', 'Crimson Web']
+    },
+    
+    'Rifles': {
+        'AK-47': ['Asiimov', 'Redline', 'Vulcan', 'Bloodsport', 'Neon Rider'],
+        'M4A4': ['Asiimov', 'Howl', 'Desolate Space', 'Poseidon', 'Neo-Noir'],
+        'M4A1-S': ['Hyper Beast', 'Icarus Fell', 'Cyrex', 'Printstream', 'Nightmare'],
+        'AWP': ['Dragon Lore', 'Asiimov', 'Medusa', 'Fade', 'Graphite'],
+        'Galil AR': ['Chatterbox', 'Eco', 'Cerberus', 'Sugar Rush'],
+        'FAMAS': ['Pulse', 'Neural Net', 'Roll Cage', 'Afterimage']
+    },
+    
+    'Pistols': {
+        'Desert Eagle': ['Blaze', 'Fade', 'Crimson Web', 'Kumicho Dragon'],
+        'USP-S': ['Kill Confirmed', 'Neo-Noir', 'Cortex', 'Blueprint'],
+        'Glock-18': ['Fade', 'Water Elemental', 'Gamma Doppler', 'Twilight Galaxy'],
+        'P250': ['Asiimov', 'Mehndi', 'Visions', 'X-Ray'],
+        'Five-SeveN': ['Neon Kimono', 'Hyper Beast', 'Monkey Business']
+    },
+    
+    'SMGs': {
+        'MP9': ['Hypnotic', 'Ruby Poison Dart', 'Airlock'],
+        'MAC-10': ['Neon Rider', 'Graven', 'Carnivore'],
+        'MP7': ['Bloodsport', 'Nemesis', 'Anodized Navy'],
+        'UMP-45': ['Blaze', 'Fade', 'Primal Saber']
+    },
+    
+    'Heavy': {
+        'Nova': ['Hyper Beast', 'Koi', 'Gila'],
+        'XM1014': ['Seasons', 'Scumbria', 'Ziggy'],
+        'MAG-7': ['Bulldozer', 'Heat', 'Petroglyph'],
+        'Sawed-Off': ['Wasteland Princess', 'Morris', 'The Kraken']
+    },
+    
+    # Machine Guns
+    'Machine Guns': {
+        'M249': ['Nebula Crusader', 'System Lock', 'Deep Relief'],
+        'Negev': ['Bratatat', 'Loudmouth', 'Power Loader']
+    }
+}
